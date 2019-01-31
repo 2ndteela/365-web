eec916a6f9a8dd45d227c94f58472b27295181f8
Jeremy Teela <jteela@ls.byu.edu>
2019-01-31 12:41:19 -0700

    Styling home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import MaterialIcon, {colorPalette} from 'material-icons-react'
+import {Link} from 'react-router-dom'
 
 import './style.css'
 
@@ -14,9 +15,22 @@ class Home extends Component {
             <div style={{height: '100%'}}>
                 <div className="row-flex" style={{justifyContent: 'space-between', height: '100%'}}>
                     <div className="col-flex" id="user-menu">
-                        <MaterialIcon icon="face" size={280} ></MaterialIcon>
+                        <MaterialIcon icon="account_circle" size={280} ></MaterialIcon>
+                        <div id="home-links" className="col-flex">
+                            <div>
+                                <div>
+                                    <Link to="/pets"><h2>Manage Pets</h2></Link>
+                                </div>
+                                <div>
+                                    <Link to="/account"><h2>Manage Account</h2></Link>
+                                </div>
+                                <div>
+                                    <Link to="/messages"><h2>Chats</h2></Link>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div id="timeline"></div>
+                    <div id="timeline" className="col-flex"></div>
                 </div>
             </div>
          );
