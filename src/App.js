ae3eee15003329cb301ca5d65fcce7e444c371a2
Branden Foley <brandenfoley@gmail.com>
2019-02-04 10:38:33 -0700

    added menu option, placeholder image

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Home from './pages/Home/'
 import Account from './pages/Account'
 import Messages from './pages/Messages'
-import Pets from  './pages/Pets'
+import Pets from './pages/Pets'
+import logo from './paw-small-68.jpg'
 
 class App extends Component {
   render() {
@@ -13,29 +14,33 @@ class App extends Component {
           <div id="router-div">
             <div id="header">
               <div id="header-content">
-                  <h1>Sitter Space</h1>
-                  <div className="row-flex">
-                    <div className="header-link">
-                      <Link to="/messages">Messages</Link>
-                    </div>
-                    <div className="header-link">
-                      <Link to="/account">Account</Link>
-                    </div>
-                    <div className="header-link">
-                      <Link to="/pets">Pets</Link>
-                    </div>
-                    <div className="header-link">
-                      <Link to="/jobs">Jobs</Link>
-                    </div>
+                <Link to="/"><img src={logo} alt="Logo" /></Link>
+                <h1>Sitter Space</h1>
+                <div className="row-flex">
+                  <div className="header-link">
+                    <Link to="/">Home</Link>
                   </div>
+                  <div className="header-link">
+                    <Link to="/messages">Messages</Link>
+                  </div>
+                  <div className="header-link">
+                    <Link to="/account">Account</Link>
+                  </div>
+                  <div className="header-link">
+                    <Link to="/pets">Pets</Link>
+                  </div>
+                  <div className="header-link">
+                    <Link to="/jobs">Jobs</Link>
+                  </div>
+                </div>
               </div>
             </div>
-              <div id='page-content' >
-                <Route exact path="/" component={Home}></Route>
-                <Route path="/account" component={Account}></Route>
-                <Route path="/messages" component={Messages}></Route>
-                <Route path="/pets" component={Pets} ></Route>
-              </div>
+            <div id='page-content' >
+              <Route exact path="/" component={Home}></Route>
+              <Route path="/account" component={Account}></Route>
+              <Route path="/messages" component={Messages}></Route>
+              <Route path="/pets" component={Pets} ></Route>
+            </div>
           </div>
         </Router>
       </div>
