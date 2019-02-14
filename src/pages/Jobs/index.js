d39e2aa177129e6209d0e89ed0a5d3170554de78
Branden Foley <brandenfoley@gmail.com>
2019-02-13 21:53:39 -0700

    yee haw

--- src/pages/Jobs/index.js
+++ src/pages/Jobs/index.js
@@ -1,33 +1,19 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './style.css'
 
 class Jobs extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            jobdata: [{
-                from: 'Job1',
-                messages: []
-            },
-            {
-                from: 'Job2',
-                messages: [],
-                current: true
-            },
-            {
-                from: 'Job3',
-                messages: []
-            }
-        ],
-         }
+        this.state = {
+        }
     }
 
     setActive(idx) {
         const temp = this.state.jobdata
 
         temp.forEach(jobdata => {
-            if(jobdata.current) jobdata.current = false
+            if (jobdata.current) jobdata.current = false
         })
 
         temp[idx].current = true
@@ -37,45 +23,21 @@ class Jobs extends Component {
         })
     }
 
-    render() { 
-        return ( 
-        <div style={{height: '100%'}} className="row-flex">
-            <div id="jobs-menu">
-                <div id="back-home">
-                    <Link to="/" >Home</Link>
+    render() {
+        return (
+            <div style={{ height: '100%' }} className="row-flex">
+                <div id="jobs-menu">
+                    <div id="back-home">
+                        <Link to="/" >Home</Link>
+                    </div>
                 </div>
-                <div>
-                {
-                    this.state.chats.map((chat, itr) => {
-                        if(chat.current) {
-                            return (
-                            <div className="conversation-header selected">
-                                <div style={{height: '36px', width: '36px', borderRadius: '18px', backgroundColor: 'grey'}}></div>
-                                <div className="convo-name">{chat.from}</div>
-                            </div>
-                            )
-                        }
-                        else {
-                            return(
-                            <div className="jpb-header" onClick={() => this.setActive(itr)}>
-                                <div style={{height: '36px', width: '36px', borderRadius: '18px', backgroundColor: 'grey'}}></div>
-                                <div className="convo-name">{chat.from}</div>
-                            </div>
-                            )
-                        }
-                    })
-                }
+                <div id="jobs">
+                    <div id="jobs-composer">
+                    </div>
                 </div>
             </div>
-            <div id="jobs">
-                <div id="jobs-composer">
-                    <input></input>
-                    <button className='primary-button'>Send</button>
-                </div>
-            </div>
-        </div>
         )
     }
 }
- 
+
 export default Jobs;
